495fe3957f65f7b53a044449ee92ca0b64a70a63
luoyunlingyu <160444676+luoyunlingyu@users.noreply.github.com>
2024-05-15 12:21:38 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# 你的Bot的Token
+TOKEN = 'YOUR_BOT_TOKEN'
+
+def start(update, context):
+    """发送一条欢迎消息当命令/start被 issued."""
+    update.message.reply_text('Hi! I am a simple Telegram Bot. Send me a message and I will reply the same to you.')
+
+def echo(update, context):
+    """回复用户发送的消息."""
+    update.message.reply_text(update.message.text)
+
+def main():
+    """启动机器人."""
+    updater = Updater(TOKEN, use_context=True)
+
+    # 获取dispatcher以注册处理器
+    dp = updater.dispatcher
+
+    # 注册处理器
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # 开始轮询
+    updater.start_polling()
+
+    # 运行直到按下Ctrl-C或程序被进程杀死
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
